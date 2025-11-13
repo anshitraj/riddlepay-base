70fa578bdb7342dbfa2b9fa3fa185c1f34f3000a
anshitraj <anshitraj4@gmail.com>
2025-11-13 19:04:25 +0530

    Increase search bar left margin to prevent sidebar overlap on desktop

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -143,7 +143,7 @@ export default function Header() {
         </div>
 
         {/* Center: Search Bar (Desktop) - Properly spaced */}
-        <div className="hidden md:flex flex-1 justify-center items-center max-w-lg mx-4 ml-12">
+        <div className="hidden md:flex flex-1 justify-center items-center max-w-lg mx-4 lg:ml-16">
           <SearchBar />
         </div>
 
